00863951b5eaa82bb72086a1ea42918fb567277d
Kelsirae <dumbandroid@gmail.com>
2019-04-22 17:39:09 -0700

    Add some init scripts

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -4,14 +4,22 @@ include $(CLEAR_VARS)
 LOCAL_MODULE        := fstab.qcom
 LOCAL_MODULE_TAGS   := optional eng
 LOCAL_MODULE_CLASS  := ETC
-LOCAL_SRC_FILES     := etc/fstab.qcom
+LOCAL_SRC_FILES     := fstab.qcom
 LOCAL_MODULE_PATH   := $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.target.rc
+LOCAL_MODULE       := init.batteryd.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.target.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init
+LOCAL_SRC_FILES    := init.batteryd.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ROOT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.mi_thermald.rc
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := init.mi_thermald.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ROOT)
 include $(BUILD_PREBUILT)
